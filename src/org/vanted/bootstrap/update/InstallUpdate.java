293b8a084232235c0825e02a70c69b6e39c59197
Matthias Klapperstueck <m.klapperstueck@gmail.com>
2015-08-31 12:57:43 +1000

    improvement of UI - added vanted logo - added error reporting

--- src/org/vanted/bootstrap/update/InstallUpdate.java
+++ src/org/vanted/bootstrap/update/InstallUpdate.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URLClassLoader;
@@ -19,12 +20,14 @@ import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
 
 import edu.monash.vanted.boot.VantedBootstrap;
 
@@ -59,115 +62,132 @@ public class InstallUpdate {
 	/**
 	 * 
 	 */
-	public static void doUpdate() throws IOException {
-		
+	public static void doUpdate()
+			throws IOException
+	{
 		File fileUpdateOK = new File(VANTEDUPDATEOKFILE);
 		ProgressDialog progressDialog = new ProgressDialog(fileUpdateOK);
-		
-		//directory was not deleted but update was done (since this check-file is here
-		if (fileUpdateOK.exists()) {
-			System.out.println("found previous update file");
-			return;
-		}
-		System.out.println("getting execution path");
-		String executionpath = VantedBootstrap.getExectutionPath(((URLClassLoader) Thread.currentThread().getContextClassLoader()).getURLs());
-		
-		List<String> listAddCoreJarRelativePath = new ArrayList<String>();
-		List<String> listRemoveCoreJarRelativePath = new ArrayList<String>();
-		List<String> listAddLibsJarRelativePaths = new ArrayList<String>();
-		List<String> listRemoveLibsJarRelativePaths = new ArrayList<String>();
-		String version;
-		progressDialog.logLine("reading update file:");
-		
-		BufferedReader reader = new BufferedReader(new FileReader(new File(DESTUPDATEFILE)));
-		System.out.println("reading update file:");
-		String line;
-		while ((line = reader.readLine()) != null) {
-			if (line.equals("//"))
-				break;
-			if (line.startsWith("#"))
-				continue;
-			if (line.toLowerCase().startsWith(VERSIONSTRING)) {
-				version = line.substring(VERSIONSTRING.length() + 1).trim();
-				progressDialog.logLine("updating to: " + version);
+		try {
+			
+			//directory was not deleted but update was done (since this check-file is here
+			if (fileUpdateOK.exists()) {
+				System.out.println("found previous update file");
+				return;
 			}
-			// look for jars to add
-			if (line.toLowerCase().startsWith("+")) {
-				line = line.substring(1);
-				if (line.toLowerCase().startsWith(CORESTRING)) {
-					listAddCoreJarRelativePath.add(line.substring(CORESTRING.length() + 1).trim());
+			System.out.println("getting execution path");
+			String executionpath = VantedBootstrap.getExectutionPath(((URLClassLoader) Thread.currentThread().getContextClassLoader()).getURLs());
+			
+			List<String> listAddCoreJarRelativePath = new ArrayList<String>();
+			List<String> listRemoveCoreJarRelativePath = new ArrayList<String>();
+			List<String> listAddLibsJarRelativePaths = new ArrayList<String>();
+			List<String> listRemoveLibsJarRelativePaths = new ArrayList<String>();
+			String version;
+			progressDialog.logLine("reading update file:");
+			
+			BufferedReader reader = new BufferedReader(new FileReader(new File(DESTUPDATEFILE)));
+			System.out.println("reading update file:");
+			String line;
+			while ((line = reader.readLine()) != null) {
+				if (line.equals("//"))
+					break;
+				if (line.startsWith("#"))
+					continue;
+				if (line.toLowerCase().startsWith(VERSIONSTRING)) {
+					version = line.substring(VERSIONSTRING.length() + 1).trim();
+					progressDialog.logLine("updating to: " + version);
 				}
-				if (line.toLowerCase().startsWith(LIBSTRING)) {
-					listAddLibsJarRelativePaths.add(line.substring(LIBSTRING.length() + 1).trim());
-				}
-			} else
-				if (line.toLowerCase().startsWith("-")) {
+				// look for jars to add
+				if (line.toLowerCase().startsWith("+")) {
 					line = line.substring(1);
 					if (line.toLowerCase().startsWith(CORESTRING)) {
-						listRemoveCoreJarRelativePath.add(line.substring(CORESTRING.length() + 1).trim());
+						listAddCoreJarRelativePath.add(line.substring(CORESTRING.length() + 1).trim());
 					}
 					if (line.toLowerCase().startsWith(LIBSTRING)) {
-						listRemoveLibsJarRelativePaths.add(line.substring(LIBSTRING.length() + 1).trim());
+						listAddLibsJarRelativePaths.add(line.substring(LIBSTRING.length() + 1).trim());
 					}
-				}
-		}
-		
-		reader.close();
-		
-		if (VantedBootstrap.DEBUG) {
-			System.out.println("installing update to :" + executionpath);
-			for (String libPath : listAddCoreJarRelativePath) {
-				System.out.println(" adding core-jar: " + libPath);
+				} else
+					if (line.toLowerCase().startsWith("-")) {
+						line = line.substring(1);
+						if (line.toLowerCase().startsWith(CORESTRING)) {
+							listRemoveCoreJarRelativePath.add(line.substring(CORESTRING.length() + 1).trim());
+						}
+						if (line.toLowerCase().startsWith(LIBSTRING)) {
+							listRemoveLibsJarRelativePaths.add(line.substring(LIBSTRING.length() + 1).trim());
+						}
+					}
 			}
-			for (String libPath : listAddLibsJarRelativePaths) {
-				System.out.println("  adding lib-jar: " + libPath);
-			}
-			System.out.println("----------");
-			for (String libPath : listRemoveCoreJarRelativePath) {
-				System.out.println(" removing core-jar: " + libPath);
-			}
-			for (String libPath : listRemoveLibsJarRelativePaths) {
-				System.out.println("  removing lib-jar: " + libPath);
-			}
-		}
-		for (String corename : listAddCoreJarRelativePath) {
-			Path source = new File(DESTPATHUPDATEDIR + corename).toPath();
-			System.out.println(source);
-			Path target = new File(executionpath + "/vanted-core/" + corename).toPath();
-			System.out.println(target);
-			Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
-			progressDialog.logLine("copied new vanted core file: " + corename);
-		}
-		for (String libname : listAddLibsJarRelativePaths) {
-			Path source = new File(DESTPATHUPDATEDIR + libname).toPath();
-			Path target = new File(executionpath + "/core-libs/" + libname).toPath();
-			Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
-			progressDialog.logLine("copied new vanted library file: " + libname);
 			
-		}
-		
-		for (String corename : listRemoveCoreJarRelativePath) {
-			File delFile = new File(executionpath + "/vanted-core/" + corename);
-			if (delFile.exists()) {
-				delFile.delete();
-				progressDialog.logLine("deleted vanted core file: " + corename);
+			reader.close();
+			
+			if (VantedBootstrap.DEBUG) {
+				System.out.println("installing update to :" + executionpath);
+				for (String libPath : listAddCoreJarRelativePath) {
+					System.out.println(" adding core-jar: " + libPath);
+				}
+				for (String libPath : listAddLibsJarRelativePaths) {
+					System.out.println("  adding lib-jar: " + libPath);
+				}
+				System.out.println("----------");
+				for (String libPath : listRemoveCoreJarRelativePath) {
+					System.out.println(" removing core-jar: " + libPath);
+				}
+				for (String libPath : listRemoveLibsJarRelativePaths) {
+					System.out.println("  removing lib-jar: " + libPath);
+				}
 			}
-		}
-		
-		for (String libname : listRemoveLibsJarRelativePaths) {
-			File delFile = new File(executionpath + "/core-libs/" + libname);
-			if (delFile.exists()) {
-				delFile.delete();
-				progressDialog.logLine("deleted vanted library file: " + libname);
+			for (String corename : listAddCoreJarRelativePath) {
+				Path source = new File(DESTPATHUPDATEDIR + corename).toPath();
+				System.out.println(source);
+				Path target = new File(executionpath + "/vanted-core/" + corename).toPath();
+				System.out.println(target);
+				Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+				progressDialog.logLine("copied new vanted core file: " + corename);
 			}
+			for (String libname : listAddLibsJarRelativePaths) {
+				Path source = new File(DESTPATHUPDATEDIR + libname).toPath();
+				Path target = new File(executionpath + "/core-libs/" + libname).toPath();
+				Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+				progressDialog.logLine("copied new vanted library file: " + libname);
+				
+			}
+			
+			for (String corename : listRemoveCoreJarRelativePath) {
+				File delFile = new File(executionpath + "/vanted-core/" + corename);
+				if (delFile.exists()) {
+					delFile.delete();
+					progressDialog.logLine("deleted vanted core file: " + corename);
+				}
+			}
+			
+			for (String libname : listRemoveLibsJarRelativePaths) {
+				File delFile = new File(executionpath + "/core-libs/" + libname);
+				if (delFile.exists()) {
+					delFile.delete();
+					progressDialog.logLine("deleted vanted library file: " + libname);
+				}
+			}
+			
+			progressDialog.logLine("done... ");
+		} catch (FileNotFoundException e) {
+			progressDialog.logLine("ERROR: " + e.getMessage());
+			progressDialog.logLine(null);
+			progressDialog.logLine("If VANTED fails to start, please "
+					+ "reinstall the latest version.");
+			
+			progressDialog.hasException();
+			
+			throw e;
+		} catch (IOException e) {
+			progressDialog.logLine("ERROR: " + e.getMessage());
+			progressDialog.logLine(null);
+			progressDialog.logLine("If VANTED fails to start, please "
+					+ "reinstall the latest version.");
+			
+			progressDialog.hasException();
+			throw e;
+		} finally {
+			progressDialog.enableClose();
 		}
-		
-		progressDialog.logLine("done... ");
-		
-		progressDialog.enableClose();
-		
-//		fileUpdateOK.createNewFile();
-//		JOptionPane.showMessageDialog(null, "Update finished");
 		
 	}
 	
@@ -188,6 +208,8 @@ public class InstallUpdate {
 		
 		File fileUpdateOK;
 		
+		private boolean hasException;
+		
 		/**
 		 * 
 		 */
@@ -198,13 +220,19 @@ public class InstallUpdate {
 			JPanel panel = new JPanel();
 			panel.setLayout(new BorderLayout());
 			
+			ImageIcon imageIcon = new ImageIcon(getClass().getResource("/org/vanted/bootstrap/update/vanted_logo_splash_16x16.png"));
 			header = new JLabel("updating..");
-			Font f = new Font("SansSerif", Font.PLAIN, 20);
+			header.setBorder(new EmptyBorder(5, 5, 5, 5));
+			header.setIcon(imageIcon);
+			Font f = new Font("SansSerif", Font.PLAIN, 14);
 			header.setFont(f);
+			panel.add(header, BorderLayout.NORTH);
 			
 			logscreen = new JTextArea();
 			logscreen.setEditable(false);
-			panel.add(header, BorderLayout.NORTH);
+			logscreen.setBorder(new EmptyBorder(5, 5, 5, 5));
+			logscreen.setLineWrap(true);
+			logscreen.setPreferredSize(new Dimension(300, 200));
 			JScrollPane scrollpane = new JScrollPane(logscreen);
 			scrollpane.setPreferredSize(new Dimension(300, 200));
 			panel.add(scrollpane, BorderLayout.CENTER);
@@ -215,6 +243,8 @@ public class InstallUpdate {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					instance.setVisible(false);
+					if (hasException)
+						return;
 					try {
 						getFileUpdateOK().createNewFile();
 					} catch (IOException e1) {
@@ -231,7 +261,6 @@ public class InstallUpdate {
 			setResizable(false);
 			setModal(false);
 			setAlwaysOnTop(true);
-//			setSize(200, 200);
 			pack();
 			setLocationRelativeTo(null);
 			
@@ -244,11 +273,17 @@ public class InstallUpdate {
 		}
 		
 		public void logLine(String text) {
+			if (text == null)
+				text = "";
 			logscreen.append(text + "\n");
 		}
 		
 		public void enableClose() {
 			okButton.setEnabled(true);
+		}
+		
+		public void hasException() {
+			hasException = true;
 		}
 	}
 }
